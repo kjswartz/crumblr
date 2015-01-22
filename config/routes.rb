07e0acb8d73fe1ab851f9c9daaefbeabef58b59b
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-01-22 17:53:13 -0500

    third commit
    Returning back to finish crumblr after watching vides. change link on
    about page to utilize ruby code. Add navigation links on home and about
    pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-
-  get 'pages/welcome'
+  get 'about' => 'pages#about'
+  root 'pages#welcome'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
